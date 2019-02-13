a2e36959621c783f54ef1f0c0383f3cdada9470f
Rex <rcsmie@gmail.com>
2019-02-12 23:39:57 -0800

    add tags_format helper for tags field of _form

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,17 @@
 module NotesHelper
+  def tags_format(array)
+    return if array.empty?
+    result = ""
+    array.each do |i|
+      result << (i + ",")
+    end
+    return "{" + remove_trailing_comma(result) + "}"
+  end
+
+  def remove_trailing_comma(string)
+    if string[-1] == ","
+      string = string[0...-1]
+    end
+    return string
+  end
 end
