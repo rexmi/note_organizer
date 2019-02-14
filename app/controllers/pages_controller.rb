45155453b83de2fc8534b901797b9c0653ee8c0f
Rex <rcsmie@gmail.com>
2019-02-13 23:54:52 -0800

    some trial to see how redcarpet works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  include PagesHelper
   def index
+    @s = markdown("
+      ```javascript
+      var s = 'hello world';
+      console.log(s);
+      ```")
+    puts @s
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new()
+    markdown = Redcarpet::Markdown.new(renderer, {})
+    @s = markdown.render(text).html_safe
+  end
 end
