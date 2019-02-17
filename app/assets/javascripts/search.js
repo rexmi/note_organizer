6ab0b76a894d59791b8994401bc8ef002ef9e034
Rex <rcsmie@gmail.com>
2019-02-17 00:52:15 -0800

    js for search queue

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,25 @@
+function send_search(e) {
+  if (e.which == 13 && $("#search-queue").val() != "") {
+    $.ajax({
+      type: "GET",
+      url: "/search",
+      data: {'search': $("#search-queue").val()},
+      success: () => {
+        console.log('yes');
+      }
+    });
+  }
+}
+
+$(() => {
+  $("#search-queue").focus(() => {
+    $("#search-queue").val("");
+    $("#search-queue").on("keydown", send_search);
+  });
+  $("#search-queue").on('focusout', () => {
+    $("#search-queue").off("keydown", send_search);
+    if($("#search-queue").val() == "") {
+      $("#search-queue").val("future search queue");
+    }
+  });
+});
