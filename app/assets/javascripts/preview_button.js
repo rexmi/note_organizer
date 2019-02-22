6d582fa694dc2c03d2aa676319c05de051f42bae
Rex <rcsmie@gmail.com>
2019-02-21 21:19:03 -0800

    remove junk

--- app/assets/javascripts/preview_button.js
+++ app/assets/javascripts/preview_button.js
@@ -8,7 +8,16 @@ function create_markdown_preview(text) {
     }
   });
 }
-
-function hello_world() {
-  alert('hello world');
-}
+//
+// function hello_world() {
+//   alert('hello world');
+// }
+//
+// function filter_for_tags() {
+//   let result = "";
+//   result = $('#note_tags').val().replace(/,\s/g, ",");
+//   if (!(result.includes("{") && result.includes("}"))) {
+//     result = "{" + result + "}";
+//   }
+//   $('#note_tags').val(result);
+// }
