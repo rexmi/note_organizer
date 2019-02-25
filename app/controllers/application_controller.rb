14e82fdd4a65e4890299c6ab6d1550dcfe8ad67e
Rex <rcsmie@gmail.com>
2019-02-25 01:32:43 -0800

    add exception handling for access_denied cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, :alert => excpetion.message }
+    end
+  end
 end
