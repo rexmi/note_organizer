bb871502f47a72727dd878c26552fae5ba1c52dd
Rex <rcsmie@gmail.com>
2019-02-25 16:09:05 -0800

    add cancancan rescue for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden }
-      format.html { redirect_to root_path, :alert => excpetion.message }
+      format.html { redirect_to '/notes', notice: exception.message }
+      # p exception.message
     end
   end
 end
